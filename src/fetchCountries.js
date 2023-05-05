36feb0645e42aa0f4a68c745f850b5d84e403093
kutsarvasya <vkutsar89@gmail.com>
2023-05-05 23:41:14 +0200

    home work

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,9 @@
 export function fetchCountries(name) {
-    return fetch(name).then(res=>res.json())
+    const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    return fetch(url).then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+       throw new Error('Oops, there is no country with that name');
+    })
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce'
+console.log(debounce);
 
-
-const url = 'https://restcountries.com/v3.1/all?fields=name,flags'
-
-fetchCountries(url)
-
-
-
+const inputEl = document.querySelector('#search-box')
+const countryListEl = document.querySelector('.country-list')
+const countryItemEl = document.querySelector('.country-info')
 const DEBOUNCE_DELAY = 300;
+    
+    
+inputEl.addEventListener('input', debounce((e) => {
+    const countryName = e.target.value.trim();
+    countryListEl.innerHTML = ''
+    countryItemEl.innerHTML = ''
+    if (!countryName) return
+
+    fetchCountries(countryName).then(countries => createMarcUp(countries)).catch(error => { Notiflix.Notify.failure(`${error}`) })
+},DEBOUNCE_DELAY))
+
+
+
+
+function createMarcUp(countries) {
+     if (countries.length > 10) {
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+        }
+        if (countries.length >= 2 && countries.length <= 10) {
+            const listCountries = countries.map((country) => {
+            return `  <li class="country-item">
+        <img src=${country.flags.svg} alt="" width="30" />
+        <h3>${country.name.common}</h3>
+      </li>`
+            }).join("")
+          countryListEl.innerHTML = listCountries
+        }
+        if (countries.length === 1) {
+            const itemCountry = `<div class="country-item">
+        <img src=${countries[0].flags.svg} alt="" width="30" />
+        <h2>${countries[0].name.common}</h2>
+      </div>
+      <ul>
+        <li><b>Capital: </b>${countries[0].capital}</li>
+        <li><b>Population: </b>${countries[0].population}</li>
+        <li><b>Languages: </b>${Object.values(countries[0].languages)}</li>
+      </ul>`
+            
+         countryItemEl.innerHTML = itemCountry
+        }
+       
+}
