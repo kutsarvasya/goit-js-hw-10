5a209117b5f363b66bd23e90ab2fd2a089ae8652
kutsarvasya <91209048+kutsarvasya@users.noreply.github.com>
2023-05-05 15:24:48 +0000

    Deploying to gh-pages from @ kutsarvasya/goit-js-hw-10@386832f21dbff342e87bc61552e1f7c500ab5b1c 🚀

---
+++ index.20cd9f7a.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/all?fields=name,flags").then((function(t){return t.json()}));
+//# sourceMappingURL=index.20cd9f7a.js.map
---
+++ index.bcfb955a.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/all?fields=name,flags").then((e=>e.json()));
+//# sourceMappingURL=index.bcfb955a.js.map
